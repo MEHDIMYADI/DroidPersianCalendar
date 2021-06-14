3a2e139ce9d73d25c5b413694617449262847a8e
Farhad Beigirad <f.beigirad@gmail.com>
2021-06-14 10:22:52 +0430

    reduce icon view hierarchy

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/ui/preferences/IconsDemoDialog.kt
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/ui/preferences/IconsDemoDialog.kt
@@ -1,10 +1,8 @@
 package com.byagowi.persiancalendar.ui.preferences
 
 import android.graphics.Color
-import android.graphics.drawable.GradientDrawable
 import android.view.ViewGroup
 import android.widget.FrameLayout
-import android.widget.ImageView
 import androidx.appcompat.app.AlertDialog
 import androidx.core.view.setPadding
 import androidx.fragment.app.Fragment
@@ -14,6 +12,8 @@ import com.byagowi.persiancalendar.R
 import com.byagowi.persiancalendar.utils.createStatusIcon
 import com.byagowi.persiancalendar.utils.dp
 import com.byagowi.persiancalendar.utils.getDayIconResource
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.shape.ShapeAppearanceModel
 
 // Debug only dialog to check validity of dynamic icons generation
 fun Fragment.showIconsDemoDialog() {
@@ -27,22 +27,21 @@ fun Fragment.showIconsDemoDialog() {
                 override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
                     object : RecyclerView.ViewHolder(FrameLayout(context).also { frameLayout ->
                         frameLayout.setPadding(4.dp)
-                        frameLayout.addView(ImageView(context).also { imageView ->
-                            val gradientDrawable = GradientDrawable()
-                            gradientDrawable.cornerRadius = 10.dp.toFloat()
-                            gradientDrawable.setColor(Color.BLACK)
-                            imageView.background = gradientDrawable
-                            imageView.layoutParams = ViewGroup.LayoutParams(36.dp, 36.dp)
-                        })
-                        frameLayout.addView(ImageView(context).also { imageView ->
+                        ShapeableImageView(context).apply {
+                            layoutParams = ViewGroup.LayoutParams(36.dp, 36.dp)
+                            setBackgroundColor(Color.BLACK)
+                            // must be applied via or extend theme shape appearance
+                            shapeAppearanceModel = ShapeAppearanceModel.Builder()
+                                .setAllCornerSizes(10.dp.toFloat())
+                                .build()
+
                             val day = viewType / 2 + 1
-                            if (viewType % 2 == 0) {
-                                imageView.setImageResource(getDayIconResource(day))
+                            if (viewType.mod(2) == 0) {
+                                setImageResource(getDayIconResource(day))
                             } else {
-                                imageView.setImageBitmap(createStatusIcon(context, day))
+                                setImageBitmap(createStatusIcon(context, day))
                             }
-                            imageView.layoutParams = ViewGroup.LayoutParams(36.dp, 36.dp)
-                        })
+                        }.also(frameLayout::addView)
                     }) {}
             }
             it.layoutManager = GridLayoutManager(context, 8)
